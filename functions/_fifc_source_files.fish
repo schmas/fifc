ac913da0f3305f324ec385a351a2f1b99773d76c
Diego Schmidt <dschmidt@national.aaa.com>
2026-01-08 17:06:56 -0300

    feat: add sorting for file output to prioritize non-hidden files

--- functions/_fifc_source_files.fish
+++ functions/_fifc_source_files.fish
@@ -6,6 +6,9 @@ function _fifc_source_files -d "Return a command to recursively find files"
         set -e fifc_query
     end
 
+    # Sort function: hidden files/folders last (strip ANSI codes for sorting)
+    set -l sort_cmd "perl -pe 's/\\e\\[[0-9;]*m//g' | awk '{if (\$0 ~ /^\\./ || \$0 ~ /\\/\\./) print \"1\" \$0; else print \"0\" \$0}' | sort | cut -c2-"
+
     if type -q fd
         if _fifc_test_version (fd --version) -ge "8.3.0"
             set fd_custom_opts --strip-cwd-prefix
@@ -20,21 +23,21 @@ function _fifc_source_files -d "Return a command to recursively find files"
         end
 
         if test "$path" = {$PWD}/
-            echo "fd . $fifc_fd_opts --color=always $hidden_flag $fd_custom_opts"
+            echo "fd . $fifc_fd_opts --color=always $hidden_flag $fd_custom_opts | $sort_cmd"
         else if test "$path" = "."
-            echo "fd . $fifc_fd_opts --color=always $hidden_flag $fd_custom_opts"
+            echo "fd . $fifc_fd_opts --color=always $hidden_flag $fd_custom_opts | $sort_cmd"
         else if test -n "$hidden"
-            echo "fd . $fifc_fd_opts --color=always $hidden_flag -- $path"
+            echo "fd . $fifc_fd_opts --color=always $hidden_flag -- $path | $sort_cmd"
         else
-            echo "fd . $fifc_fd_opts --color=always $hidden_flag -- $path"
+            echo "fd . $fifc_fd_opts --color=always $hidden_flag -- $path | $sort_cmd"
         end
     else if test -n "$hidden"
         # Use sed to strip cwd prefix
-        echo "find . $path $fifc_find_opts ! -path . -print 2>/dev/null | sed 's|^\./||'"
+        echo "find . $path $fifc_find_opts ! -path . -print 2>/dev/null | sed 's|^\./||' | $sort_cmd"
     else
         # Exclude hidden directories unless fifc_show_hidden is enabled
         if set -q fifc_show_hidden; and test "$fifc_show_hidden" = true
-            echo "find . $path $fifc_find_opts ! -path . -print 2>/dev/null | sed 's|^\./||'"
+            echo "find . $path $fifc_find_opts ! -path . -print 2>/dev/null | sed 's|^\./||' | $sort_cmd"
         else
             echo "find . $path $fifc_find_opts ! -path . ! -path '*/.*' -print 2>/dev/null | sed 's|^\./||'"
         end
